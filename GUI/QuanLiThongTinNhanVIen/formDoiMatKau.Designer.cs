7f04f0ddf9e21aa40e6a343016bf8a854d288aef
Long Ho <longcailay@gmail.com>
2019-01-04 09:31:21 +0700

    Hoan thanh thay doi mat khau

--- GUI/QuanLiThongTinNhanVIen/formDoiMatKau.Designer.cs
+++ GUI/QuanLiThongTinNhanVIen/formDoiMatKau.Designer.cs
@@ -39,6 +39,7 @@
             this.txbMKXacNhan = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.txtLoiXacNhan = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -146,11 +147,23 @@
             this.txtLoiXacNhan.TabIndex = 13;
             this.txtLoiXacNhan.Text = "Mật khẩu xác nhận không trùng khớp!";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(41, 306);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(451, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Ít nhất 8 kí tự, có chữ số, chữ hoa, chữ thường, không chứa kí tự đặc biệt";
+            // 
             // formDoiMatKau
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(504, 406);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtLoiXacNhan);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txbMKXacNhan);
@@ -189,5 +202,6 @@
         private System.Windows.Forms.TextBox txbMKXacNhan;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label txtLoiXacNhan;
+        private System.Windows.Forms.Label label6;
     }
 }
--- GUI/QuanLiThongTinNhanVIen/formDoiMatKau.cs
+++ GUI/QuanLiThongTinNhanVIen/formDoiMatKau.cs
@@ -46,6 +46,7 @@ namespace GUI.QuanLiThongTinNhanVIen
             txbTaiKhoan.Text = dttb.Rows[0][0].ToString();
            m_matKhauCu = dttb.Rows[0][1].ToString();
             txtLoiXacNhan.Hide();
+            label6.Hide();
            // txbMatKhauMoi.Text= dttb.Rows[0][2].ToString();
         }
 
@@ -59,6 +60,36 @@ namespace GUI.QuanLiThongTinNhanVIen
 
         }
 
+        static bool ValidatePassword(string password)//nguồn :https://stackoverflow.com/questions/5859632/regular-expression-for-password-validation
+        {
+            const int MIN_LENGTH = 8;
+            const int MAX_LENGTH = 30;
+
+            if (password == null) throw new ArgumentNullException();
+
+            bool meetsLengthRequirements = password.Length >= MIN_LENGTH && password.Length <= MAX_LENGTH;
+            bool hasUpperCaseLetter = false;
+            bool hasLowerCaseLetter = false;
+            bool hasDecimalDigit = false;
+
+            if (meetsLengthRequirements)
+            {
+                foreach (char c in password)
+                {
+                    if (char.IsUpper(c)) hasUpperCaseLetter = true;
+                    else if (char.IsLower(c)) hasLowerCaseLetter = true;
+                    else if (char.IsDigit(c)) hasDecimalDigit = true;
+                }
+            }
+
+            bool isValid = meetsLengthRequirements
+                        && hasUpperCaseLetter
+                        && hasLowerCaseLetter
+                        && hasDecimalDigit
+                        ;
+            return isValid;
+
+        }
         private void BtnDoiMatKhau_Click(object sender, EventArgs e)
         {
             
@@ -67,20 +98,29 @@ namespace GUI.QuanLiThongTinNhanVIen
             
             if (matKhauCu == m_matKhauCu)
             {
+                label6.Hide();
                 if (txbMatKhauMoi.Text == txbMKXacNhan.Text)
                 {
                     txtLoiXacNhan.Hide();
-                    //tạo dto
-                    DTO_Account AC = new DTO_Account(0, "", matKhauMoi, 0, m_MaNhanVien);
-                    if (BUS_AC.suaAccount(AC))
+                    if (ValidatePassword(txbMatKhauMoi.Text))
                     {
-                        MessageBox.Show("Mật khẩu thay đổi thành công!", "Successful", MessageBoxButtons.OK);
-                        this.Hide();
+                        //tạo dto
+                        DTO_Account AC = new DTO_Account(0, "", matKhauMoi, 0, m_MaNhanVien);
+                        if (BUS_AC.suaAccount(AC))
+                        {
+                            MessageBox.Show("Mật khẩu thay đổi thành công!", "Successful", MessageBoxButtons.OK);
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Thay đổi thất bại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                        }
                     }
                     else
                     {
-                        MessageBox.Show("Thay đổi thất bại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-
+                        //MessageBox.Show("Mật khẩu không quá ngắn, không chứa kí tự đặc biệt!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        label6.Show();
                     }
                 }
                 else
